f9b8a6b18bf943aa8a10bab6a9cdcdf0306e388a
Ian Hitchman <131753725+ianh8899@users.noreply.github.com>
2026-09-06 21:19:44 +0100

    fix: typos

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -165,7 +165,7 @@ export const projectsData: {
     index: 1,
     title: "Flaude AI App",
     description:
-      "A proof of concept AI application based on Clauda.ai utilising a full stack of technologies to deliver a modern AI saas application.",
+      "A proof of concept AI application based on Clauda.ai utilising a full stack of technologies to deliver a modern AI SaaS application.",
     features: [
       "Frontend: The application's client is built in React, TypeScript brings a layer of type safety, ensuring a robust application with fewer runtime errors.",
       "Backend: Hono, running within the Node.js environment, managing server-side operations, routing, and data handling.",
